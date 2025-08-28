ffda12eb6755051fd2916247dce10533dec027da
Riyaz Hasan <riyaz.hasan@intellier.com>
2025-08-29 02:05:45 +0600

    testing with github ci-cd

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hello DevOps World 🌍🚀" });
+  res.json({ message: "Hello DevOps World  Welcome🌍🚀" });
 });
 
 app.get("/health", (req, res) => {
